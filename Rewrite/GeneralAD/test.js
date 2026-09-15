55cafa11192d204bd9e3c04a1ca3095a7e9cab02
s1uBin <55728377+s1uBin@users.noreply.github.com>
2026-09-15 22:54:13 +0800

    Update test.js

--- Rewrite/GeneralAD/test.js
+++ Rewrite/GeneralAD/test.js
@@ -2,17 +2,17 @@ var url = $request.url;
 var status = $response.status;
 var body = $response.body;
 
-console.log("=== TOY TEST ===");
+console.log("=== TOY HOME CHECK ===");
 console.log("URL: " + url);
 console.log("STATUS: " + status);
 console.log("BODY LENGTH: " + (body ? body.length : 0));
 
-if (body) {
-    console.log("listAD: " + (body.indexOf("listAD") >= 0));
-    console.log("footerAD: " + (body.indexOf("footerAD") >= 0));
-    console.log("bottomFixedBanner: " + (body.indexOf("bottomFixedBanner") >= 0));
-    console.log("GPT: " + (body.indexOf("div-gpt-ad-") >= 0));
-    console.log("SWIPER: " + (body.indexOf("toy-Swiper") >= 0));
+if (body && body.length > 1000) {
+    console.log("HAS HTML: " + (body.indexOf("<html") >= 0));
+    console.log("HAS listAD: " + (body.indexOf("listAD") >= 0));
+    console.log("HAS footerAD: " + (body.indexOf("footerAD") >= 0));
+    console.log("HAS GPT: " + (body.indexOf("div-gpt-ad-") >= 0));
+    console.log("HAS SWIPER: " + (body.indexOf("toy-Swiper") >= 0));
 }
 
 console.log("=== END ===");
