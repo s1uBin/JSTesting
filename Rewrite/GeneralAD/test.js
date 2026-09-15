25601c16d9130a0ea64ad3174e018d524143de7c
s1uBin <55728377+s1uBin@users.noreply.github.com>
2026-09-15 22:03:36 +0800

    Update test.js

--- Rewrite/GeneralAD/test.js
+++ Rewrite/GeneralAD/test.js
@@ -1,9 +1,27 @@
 var body = $response.body;
+if (!body) { $done({}); } else {
+// 只給 Google 廣告容器加 display:none
+body = body.replace(
+    /(<div[^>]+id=['"]div-gpt-ad-1548832076097-0['"][^>]*)(>)/i,
+    '$1 style="display:none!important"$2'
+);
 
-if (!body) {
-    $done({});
-} else {
-    $done({
-        body: '{"round":1,"status":"null","data":null}'
-    });
+body = body.replace(
+    /(<div[^>]+id=['"]div-gpt-ad-1548832129106-0['"][^>]*)(>)/i,
+    '$1 style="display:none!important"$2'
+);
+
+body = body.replace(
+    /(<div[^>]+id=['"]div-gpt-ad-1548832180624-0['"][^>]*)(>)/i,
+    '$1 style="display:none!important"$2'
+);
+
+body = body.replace(
+    /(<div[^>]+id=['"]div-gpt-ad-1609243813618-0['"][^>]*)(>)/i,
+    '$1 style="display:none!important"$2'
+);
+
+$done({
+    body: body
+});
 }
