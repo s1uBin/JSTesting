8e89e9d73c461bbbdb59a5e889132c0681920ae2
s1uBin <55728377+s1uBin@users.noreply.github.com>
2026-09-15 16:57:37 +0800

    Update test.js

--- Rewrite/GeneralAD/test.js
+++ Rewrite/GeneralAD/test.js
@@ -1,24 +1,17 @@
 var body = $response.body;
 if (!body) { $done({}); } else {
-// 只移除確定是廣告的區塊
-body = body.replace(
-    /<div[^>]*class=["'][^"']*\blistAD\b[^"']*["'][\s\S]*?<\/div>\s*<\/div>/gi,
-    ""
-);
+var css =
+    '<style id="loon-toy-ad-clean">' +
+    '.listAD,' +
+    '.footerAD,' +
+    '.bottomFixedBanner' +
+    '{display:none!important;}' +
+    '</style>';
 
 body = body.replace(
-    /<div[^>]*class=["'][^"']*\bfooterAD\b[^"']*["'][\s\S]*?<\/div>\s*<\/div>/gi,
-    ""
+    '</head>',
+    css + '</head>'
 );
-
-body = body.replace(
-    /<div[^>]*class=["'][^"']*\bbottomFixedBanner\b[^"']*["'][\s\S]*?<\/div>\s*<\/div>\s*<\/div>/gi,
-    ""
-);
-
-// 不再刪除 div-gpt-ad
-// 不再刪除 iframe
-// 不處理 img
 
 $done({
     body: body
