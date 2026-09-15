9880fb579fbec4f15240cb5d75cca1842ac4c353
s1uBin <55728377+s1uBin@users.noreply.github.com>
2026-09-15 23:06:11 +0800

    Update test.js

--- Rewrite/GeneralAD/test.js
+++ Rewrite/GeneralAD/test.js
@@ -1,39 +1,68 @@
 var body = $response.body;
 if (!body) { $done({}); } else {
-// Remove Toy-People ad containers only
+/*
+ * ============================================================
+ * Toy-People Ad Cleaner
+ * Only remove known ad containers.
+ * Do NOT touch toy-Swiper / swiper-slide / img.toy-people.com
+ * ============================================================
+ */
+
+// 1. Known Toy-People ad blocks
 body = body.replace(
-    /<div[^>]*class=["'][^"']*\blistAD\b[^"']*["'][\s\S]*?<\/div>\s*<\/div>/gi,
+    /<div\b[^>]*class\s*=\s*["'][^"']*\blistAD\b[^"']*["'][^>]*>[\s\S]*?<\/div>\s*<\/div>/gi,
     ""
 );
 
 body = body.replace(
-    /<div[^>]*class=["'][^"']*\bfooterAD\b[^"']*["'][\s\S]*?<\/div>\s*<\/div>/gi,
+    /<div\b[^>]*class\s*=\s*["'][^"']*\bfooterAD\b[^"']*["'][^>]*>[\s\S]*?<\/div>\s*<\/div>/gi,
     ""
 );
 
 body = body.replace(
-    /<div[^>]*class=["'][^"']*\bbottomFixedBanner\b[^"']*["'][\s\S]*?<\/div>\s*<\/div>\s*<\/div>/gi,
+    /<div\b[^>]*class\s*=\s*["'][^"']*\bbottomFixedBanner\b[^"']*["'][^>]*>[\s\S]*?<\/div>\s*<\/div>\s*<\/div>/gi,
     ""
 );
 
-// Remove Google GPT ad slots only
+// 2. Known Google GPT containers
 body = body.replace(
-    /<div[^>]*id=["']div-gpt-ad-1548832076097-0["'][^>]*>[\s\S]*?<\/div>/gi,
-    ""
-);
-
-body = body.replace(
-    /<div[^>]*id=["']div-gpt-ad-1548832129106-0["'][^>]*>[\s\S]*?<\/div>/gi,
+    /<div\b[^>]*id\s*=\s*["']div-gpt-ad-1548832076097-0["'][^>]*>[\s\S]*?<\/div>/gi,
     ""
 );
 
 body = body.replace(
-    /<div[^>]*id=["']div-gpt-ad-1548832180624-0["'][^>]*>[\s\S]*?<\/div>/gi,
+    /<div\b[^>]*id\s*=\s*["']div-gpt-ad-1548832129106-0["'][^>]*>[\s\S]*?<\/div>/gi,
     ""
 );
 
 body = body.replace(
-    /<div[^>]*id=["']div-gpt-ad-1609243813618-0["'][^>]*>[\s\S]*?<\/div>/gi,
+    /<div\b[^>]*id\s*=\s*["']div-gpt-ad-1548832180624-0["'][^>]*>[\s\S]*?<\/div>/gi,
+    ""
+);
+
+body = body.replace(
+    /<div\b[^>]*id\s*=\s*["']div-gpt-ad-1609243813618-0["'][^>]*>[\s\S]*?<\/div>/gi,
+    ""
+);
+
+// 3. Remove GPT display calls for those four slots
+body = body.replace(
+    /googletag\.display\s* \s*;?/gi,
+    ""
+);
+
+body = body.replace(
+    /googletag\.display\s* \s*;?/gi,
+    ""
+);
+
+body = body.replace(
+    /googletag\.display\s* \s*;?/gi,
+    ""
+);
+
+body = body.replace(
+    /googletag\.display\s* \s*;?/gi,
     ""
 );
 
