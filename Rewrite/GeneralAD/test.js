bc6605eac685e1b7a160d4361e07abef7caf2759
s1uBin <55728377+s1uBin@users.noreply.github.com>
2026-09-15 17:32:46 +0800

    Update test.js

--- Rewrite/GeneralAD/test.js
+++ Rewrite/GeneralAD/test.js
@@ -1,18 +1,3 @@
-var body = $response.body;
-if (!body) { $done({}); } else {
-var css =
-    '<style id="loon-toy-ad-clean">' +
-    '.listAD { display:none !important; }' +
-    '.footerAD { display:none !important; }' +
-    '.bottomFixedBanner { display:none !important; }' +
-    '</style>';
-
-body = body.replace(
-    /<\/head>/i,
-    css + '</head>'
-);
-
 $done({
-    body: body
+    
 });
-}
