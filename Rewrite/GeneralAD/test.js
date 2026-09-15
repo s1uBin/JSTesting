f566d9b68cc8f3b4040174dabc6689b2e3d27927
s1uBin <55728377+s1uBin@users.noreply.github.com>
2026-09-15 22:23:24 +0800

    Update test.js

--- Rewrite/GeneralAD/test.js
+++ Rewrite/GeneralAD/test.js
@@ -1,27 +1,42 @@
-var body = $response.body;
-if (!body) { $done({}); } else {
-// 只給 Google 廣告容器加 display:none
-body = body.replace(
-    /(<div[^>]+id=['"]div-gpt-ad-1548832076097-0['"][^>]*)(>)/i,
-    '$1 style="display:none!important"$2'
+// Toy-People Response Diagnostic // 只讀取，不修改任何 Response
+var url = ($request && $request.url) ? $request.url : "NO_REQUEST"; var status = ($response && $response.status) ? $response.status : 0;
+var headers = ($response && $response.headers) ? $response.headers : {}; var contentType = headers["Content-Type"]  headers["content-type"]  "";
+var encoding = headers["Content-Encoding"]  headers["content-encoding"]  "";
+var body = ($response && $response.body) ? $response.body : ""; var bodyLength = body.length;
+// 只針對首頁 HTML 做內容偵測 var isHome = /^https://www.toy-people.com/(?:?.*)?$/i.test(url);
+var result = [];
+if (isHome && body) {
+result.push("HOME=YES");
+
+result.push(
+    "listAD=" +
+    ((body.match(/\blistAD\b/gi) || []).length)
 );
 
-body = body.replace(
-    /(<div[^>]+id=['"]div-gpt-ad-1548832129106-0['"][^>]*)(>)/i,
-    '$1 style="display:none!important"$2'
+result.push(
+    "footerAD=" +
+    ((body.match(/\bfooterAD\b/gi) || []).length)
 );
 
-body = body.replace(
-    /(<div[^>]+id=['"]div-gpt-ad-1548832180624-0['"][^>]*)(>)/i,
-    '$1 style="display:none!important"$2'
+result.push(
+    "bottomFixedBanner=" +
+    ((body.match(/\bbottomFixedBanner\b/gi) || []).length)
 );
 
-body = body.replace(
-    /(<div[^>]+id=['"]div-gpt-ad-1609243813618-0['"][^>]*)(>)/i,
-    '$1 style="display:none!important"$2'
+result.push(
+    "GPT=" +
+    ((body.match(/div-gpt-ad-/gi) || []).length)
 );
 
-$done({
-    body: body
-});
-}
+result.push(
+    "Swiper=" +
+    ((body.match(/toy-Swiper|swiper-slide/gi) || []).length)
+);
+
+result.push(
+    "toy-ad.php=" +
+    ((body.match(/toy-ad\.php/gi) || []).length)
+);
+} else { result.push("HOME=NO"); }
+$notification.post( "Toy-People TEST", "Status=" + status + "  Body=" + bodyLength, result.join(" | ") );
+// ★關鍵：完全不修改 Response $done({});
